8863d6a17ac92b38d6cbfbad26e24fdcba56ebde
David McClure <davidwilliammcclure@gmail.com>
2017-12-05 23:27:23 -0500

    Helpers to merge glove vocabs.

--- sources.py
+++ sources.py
@@ -4,6 +4,7 @@ import os
 import attr
 import re
 
+from collections import OrderedDict, Counter
 from glob import glob
 
 
@@ -16,3 +17,33 @@ class Corpus:
         with open(self.path) as fh:
             for line in fh:
                 yield re.findall('\w+', line.lower())
+
+
+def parse_glove_vocab(path):
+    """Parse a Glove vocab count file.
+    """
+    vocab = OrderedDict()
+
+    with open(path) as fh:
+        for line in fh:
+
+            try:
+                token, count = line.strip().split()
+                vocab[token] = int(count)
+            except Exception as e:
+                print(e)
+
+    return vocab
+
+
+def intersect_glove_vocabs(*vocabs):
+    """Intersect word types, sum counts.
+    """
+    tokens = set.intersection(*[set(v.keys()) for v in vocabs])
+
+    vocab = Counter()
+
+    for token in tokens:
+        vocab[token] = sum([v[token] for v in vocabs])
+
+    return vocab
